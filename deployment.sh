967252787a92a7b0bf27799f78275b5e8db10e41
Martin Kiesel <martin.kiesel@gmail.com>
2016-10-16 23:17:55 +0200

    Test push event #2
    Test push event #3
    
    Test push event #4
    
    Test push event #5
    
    Add deployment script
    
    Test push event #6
    
    Test push event #7
    
    Test push event #8
    
    Fix art to php artisan
    
    Permissions set for deployment.sh
    
    Test push event #9
    
    Test push event #11
    
    Remove artisan commands from deployment script
    
    Add newline at the EOF
    
    Test push event #12
    
    Test push event #13
    
    Test push event #14
    
    Autodeploy up, and running

---
+++ deployment.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git up
+composer install
+composer dump-autoload -o
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 Route::get('/', function () {
-    return view('pages.dashboard');
-    //some change, testing auto-deployment
+    return redirect()->route('login');
+    //return view('pages.dashboard');
 });
 
 /*
