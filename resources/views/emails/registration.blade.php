f9819ffb350b5544b819fc88f77ec56ceade55ee
Marek Vavrica <marek.vavrica@gmail.com>
2016-11-02 14:07:46 +0100

    email message update

--- resources/views/emails/registration.blade.php
+++ resources/views/emails/registration.blade.php
@@ -1,3 +1,9 @@
-This is an email send after registration.
+Hello { {$user->first_name} }! 
 
+You have been registred on FEIBOX portal. </br>
+
+
+Please verify your e-mail by clicking on this <a href="{{ route('account.verify', ['token' => $user->registration_token]) }}"> link </a>.</br>
+Or copy this link to your browser.</br>
 {{ route('account.verify', ['token' => $user->registration_token]) }}
+
