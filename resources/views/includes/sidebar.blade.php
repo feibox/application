fa27dce24b2761dd15e4d6336e63d46f7c441b09
Martin Kiesel <martin.kiesel@gmail.com>
2016-11-12 15:55:06 +0100

    Update sidebar

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -16,10 +16,27 @@
                         <span class="icon fa fa-tachometer"></span><span class="title">Dashboard</span>
                     </a>
                 </li>
-                <li class="{{ set_active_routes('account.password.edit') }}">
-                    <a href="{{ route('account.password.edit') }}">
-                        <span class="icon fa fa-user"></span><span class="title">Password</span>
+                <li class="{{ set_active_routes('users.index') }}">
+                    <a href="{{ route('users.index') }}">
+                        <span class="icon fa fa-users"></span><span class="title">Users</span>
                     </a>
+                </li>
+
+                <li class="panel panel-default dropdown {{ set_active_paths('account/*') }}">
+                    <a data-toggle="collapse" href="#dropdown-posts" class="{{ set_active_paths('account/*', 'collapsed') }}">
+                        <span class="icon fa fa-id-card-o"></span><span class="title">Account</span>
+                    </a>
+                    <div id="dropdown-posts" class="panel-collapse collapse {{ set_active_paths('account/*', 'in') }}">
+                        <div class="panel-body">
+                            <ul class="nav navbar-nav">
+                                <li>
+                                    <a href="{{ route('account.password.edit') }}">
+                                        <span class="icon fa fa-user"></span><span class="title">Password</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </li>
             </ul>
         </div>
