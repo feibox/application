188d9355d9a9384d7bb992b9053def7c30491529
Martin Kiesel <martin.kiesel@gmail.com>
2016-11-13 01:42:43 +0100

    Update deployment procedure

--- deployment.sh
+++ deployment.sh
@@ -3,4 +3,14 @@
 git up
 composer install
 composer dump-autoload -o
-sudo /bin/systemctl restart supervisor.service
+php /var/www/html/application/artisan down
+php /var/www/html/application/artisan clear-compiled
+php /var/www/html/application/artisan cache:clear
+php /var/www/html/application/artisan view:clear
+php /var/www/html/application/artisan config:clear
+php /var/www/html/application/artisan config:cache
+php /var/www/html/application/artisan route:clear
+php /var/www/html/application/artisan route:cache
+php /var/www/html/application/artisan optimize
+php /var/www/html/application/artisan queue:restart
+php /var/www/html/application/artisan up
